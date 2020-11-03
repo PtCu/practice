bacd1494699886d21cb89935eab85c539d7c76e9
PtCu <1365699009@qq.com>
2020-11-03 20:57:39 +0800

    1

---
+++ OJ/luogu/P1996.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    queue<int> Q;
+    for (int i = 1; i <= n; ++i)
+    {
+        Q.emplace(i);
+    }
+    int out = 0;
+    int cur = 0;
+    while (!Q.empty())
+    {
+        if (++cur == m)
+        {
+            cout << Q.front() << " ";
+            Q.pop();
+            cur = 0;
+        }
+        else
+        {
+            Q.push(Q.front());
+            Q.pop();
+        }
+    }
+}
