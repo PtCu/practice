020767f1b60c99ded4300385dac32a54c34f5fb7
PtCu <1365699009@qq.com>
2020-12-01 00:59:17 +0800

    1

--- OJ/luogu/P1229.cpp
+++ OJ/luogu/P1229.cpp
@@ -1,18 +1,20 @@
 //在先序遍历中某一元素A的后继元素B，
 //如果在后序遍历中A的前驱元素是B，
 //那么A只有一个子树，问题即得解
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+int main()
+{
     string pre, pos;
     cin >> pre >> pos;
     int ans = 0;
-    for (size_t i = 0; i < pre.size();++i)
-        for (size_t j = 0; j < pos.size();++j){
+    for (size_t i = 0; i < pre.size()-1; ++i)
+        for (size_t j = 0; j < pos.size(); ++j)
+        {
             //先序AB,后序BA的情况
-            if(pre[i]==pos[j]&&pre[i+1]==pos[j-1])
+            if (pre[i] == pos[j] && pre[i + 1] == pos[j - 1])
                 ans++;
         }
-    cout << 1<<ans;
+    cout << (1 << ans);
     return 0;
 }
