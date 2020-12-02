daba5770809f8e31a70fb91d329da323bde56fbc
PtCu <1365699009@qq.com>
2020-12-03 00:40:54 +0800

    1

--- OJ/luogu/P1102.cpp
+++ OJ/luogu/P1102.cpp
@@ -1,62 +1,24 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <map>
 using namespace std;
-int binarySearch(int goal, vector<int> &source)
+typedef long long LL;
+LL a[200001];
+map<LL, LL> m; //建立一个数字到出现次数的映射 map<num,times>
+//A-B=C --> A-C=B
+int main()
 {
-    int left = 0;
-    int right = source.size() - 1;
-    while (left <= right)
-    {
-        int mid = (left + right) / 2;
-        if (source[mid] > goal)
-        {
-            right = mid - 1;
-        }
-        else if (source[mid] < goal)
-        {
-            left = mid + 1;
-        }
-        else
-        {
-            return mid;
-        }
-    }
-    return -1;
-}
-int main(int argc, char const *argv[])
-{
-    int n, c;
+    int n;
+    LL c;
+    LL ans = 0;
     cin >> n >> c;
-    vector<int> source;
-    
-    //刚开始vector为空时不能用下标赋值，必须push_back
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        int j;
-        cin >> j;
-        source.push_back(j);
+        cin >> a[i];
+        m[a[i]]++;
+        a[i] -= c;
     }
-    sort(source.begin(),source.end());
-    int num = 0;
-    for (int i = 0; i < n; i++)
-    {
-        int index = binarySearch(c+source[i], source);
-        if (index != -1)
-        {
-            num++;
-            int lindex = index;
-            int rindex = index;
-            while (lindex > 0 && source[lindex] == source[lindex - 1])
-            {
-                lindex--;
-                num++;
-            }
-            while (rindex < source.size() - 1 && source[rindex] == source[rindex + 1])
-            {
-                rindex++;
-                num++;
-            }
-        }
-    }
-    cout << num << " ";
+    for (int i = 1; i <= n; i++)
+        ans += m[a[i]];
+    cout << ans << endl;
     return 0;
 }
