c0d19a0efd42114eaea5f6f822731fa65a84d58f
PtCu <1365699009@qq.com>
2021-01-07 23:46:23 +0800

    2

--- data structure/pm.hpp
+++ data structure/pm.hpp
@@ -17,7 +17,6 @@ int match_bf1(char *P, char *T)
             i -= j - 1;
             j = 0;
         }
-       
     }
     return i - j;
 }
@@ -37,4 +36,62 @@ int match_bf2(char *P, char *T)
             break;
     }
     return i;
+}
+
+int *buildNext(char *P)
+{
+    size_t m = strlen(P), j = 0; //“主”串指针
+    int *N = new int[m];
+    int t = N[0] = -1; //模式串指针
+    while (j < m - 1)
+    {
+        (0 > t || P[j] == P[t]) ? N[++j] = ++t : t = N[t]; //匹配、失配
+    }
+    return N;
+}
+int *buildNext_improved(char *P)
+{
+    size_t m = strlen(P), j = 0;
+    int *N = new int[m];
+    int t = N[0] = -1;
+    while (j < m - 1)
+    {
+        if (0 > t || P[j] == P[t])
+        { //匹配
+            N[j] = (P[++j] != P[++t] ? t : N[t]);
+        }
+        t = N[t];
+        /* code */
+    }
+    return N;
+}
+int match_kmp(char *P, char *T)
+{                                  //KMP算法
+    int *next = buildNext(P);      //构造next表
+    int n = (int)strlen(T), i = 0; //文本串指针
+    int m = (int)strlen(P), j = 0; //模式串指针
+    while (j < m && i < n)         //自左向右逐个比对字符
+        if (0 > j || T[i] == P[j]) //若匹配，或P已移出最左侧（两个判断的次序不可交换）
+        {
+            i++;
+            j++;
+        }                //则转到下一字符
+        else             //否则
+            j = next[j]; //模式串右移（注意：文本串不用回退）
+
+    delete[] next; //释放next表
+    return i - j;
+}
+
+int* buildBC(char* P){
+    int *bc = new int[256];
+    for (size_t j = 0; i < 256;++j)
+        bc[j] = -1;
+    for (size_t m = strlen(P), j = 0; j < m;j++)
+        bc[P[j]] = j;
+    return bc;
+}
+
+int match_bc(char*P,char* T){
+    int *bc = buildBC(P);
 }
