a47ea9b03894634d6358e9c9a616809c3a1426bf
PtCu <1365699009@qq.com>
2021-01-17 23:56:29 +0800

    2

--- OJ/luogu/P2825.cpp
+++ OJ/luogu/P2825.cpp
@@ -1,46 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int maxn = 1e4;
+const int maxn = 1e3+10;
 int N, K, M;
 vector<int> G[maxn];
-bool hasCow[maxn];
+int hasCow[maxn];
 bool visited[maxn];
-void dfs(int x, int& remain)
+int nums[maxn]; //记录每个顶点被遍历的次数
+void dfs(int x)
 {
-    if (hasCow[x])
-        remain -= 1;
+    visited[x] = true;
+    nums[x]++;
     for (size_t i = 0; i < G[x].size(); ++i)
     {
         if (!visited[G[x][i]])
         {
-            visited[G[x][i]] = true;
-            dfs(G[x][i], remain);
+            dfs(G[x][i]);
         }
     }
 }
 int main()
 {
-
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     cin >> K >> N >> M; //k个奶牛，N个牧场，M条路
     int x;
     for (size_t i = 1; i <= K; ++i)
     {
-        cin >> x;
-        hasCow[x] = true;
+        cin >> hasCow[i];
     }
     int a, b;
     for (size_t i = 0; i < M; ++i)
     {
         cin >> a >> b;
-        G[b].push_back(a);
+        G[a].push_back(b);
     }
     int ans = 0;
-    for (size_t i = 1; i <= M;++i){
+    for (size_t i = 1; i <= K; ++i)
+    {
         memset(visited, 0, sizeof(visited));
-        int remain = K;
-        dfs(i, remain);
-        if(remain==0)
-            ans++;
+        dfs(hasCow[i]);
+    }
+    for (size_t i = 1; i <= N; ++i)
+    {
+        if (nums[i] == K)
+            ++ans;
     }
     cout << ans;
 }
