485ee62f011a2a3f74a33828094fdf193a606006
PtCu <1365699009@qq.com>
2021-01-21 23:57:06 +0800

    2

--- OJ/luogu/P1983.cpp
+++ OJ/luogu/P1983.cpp
@@ -1,15 +1,38 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-const int maxn = 1e4;
+const int maxn = 1e3 + 5;
 vector<int> G[maxn];
-int main(){
+bool cache[maxn];
+vector<int> stop;
+void topo()
+{
+    
+}
+int main()
+{
     int n, m;
     cin >> n >> m;
-    int s,x;
-    for (size_t i = 0; i < m;++i){
+    int s, x;
+    for (size_t i = 0; i < m; ++i)
+    {
+        memset(cache, 0, sizeof(cache));
+        stop.clear();
         cin >> s;
-        for (size_t j = 0; j < s;++j){
-            
+        for (size_t j = 1; j <= s; ++j)
+        {
+            cin >> x;
+            stop.push_back(x);
+            cache[x] = 1;
+        }
+        for (size_t j = 1; j <= s; ++j)
+        {
+            if (!cache[j])
+            {
+                for (auto k : stop)
+                {
+                    G[k].push_back(j); //k到j的连线
+                }
+            }
         }
     }
 }
