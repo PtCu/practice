2fded94f480e94fa64f0a0c161d14439ff94e8bc
PtCu <1365699009@qq.com>
2021-01-21 23:35:37 +0800

    1

---
+++ OJ/luogu/P1983.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int maxn = 1e4;
+vector<int> G[maxn];
+int main(){
+    int n, m;
+    cin >> n >> m;
+    int s,x;
+    for (size_t i = 0; i < m;++i){
+        cin >> s;
+        for (size_t j = 0; j < s;++j){
+            
+        }
+    }
+}
