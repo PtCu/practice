c140fd48db200475bd8cce76a252112d4264d5bc
PtCu <1365699009@qq.com>
2021-02-04 23:38:12 +0800

    2

--- OJ/luogu/P2004.cpp
+++ OJ/luogu/P2004.cpp
@@ -1,2 +1,38 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
+vector<int> ans;
+struct node
+{
+    int val;
+    int num;
+};
+deque<node> Q;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n, k;
+    node x;
+    cin >> n >> k;
+    for (int i = 1; i <= n; ++i)
+    {
+        cin >> x.val;
+        x.num = i;
+        while (!Q.empty() && x.val > Q.back().val)
+        {
+            Q.pop_back();
+        }
+        Q.push_back(x);
+        while (i - Q.front().num >= k)
+        {
+            Q.pop_front();
+        }
+
+        if (i >= k)
+        {
+            cout << Q.front().val << endl;
+        }
+    }
+}
